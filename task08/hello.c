f8154c4f32ac83bcb6f1831fcc6d18dae41af8fe
Manikishan Ghantasala <manikishanghantasala@gmail.com>
2021-03-12 01:21:23 +0530

    [028aba07e56d] Task 08: add foo file

--- task08/hello.c
+++ task08/hello.c
@@ -6,6 +6,8 @@
 #include <linux/init.h>
 #include <linux/debugfs.h>
 #include <linux/jiffies.h>
+#include <linux/semaphore.h>
+#include <linux/uaccess.h>
 
 #define ID "028aba07e56d"
 #define ID_LEN 13 /* ID length */
@@ -13,6 +15,11 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("028aba07e56d");
 MODULE_DESCRIPTION("Debugfs module");
+
+static DEFINE_SEMAPHORE(foo_sem);
+
+static char foo_data[PAGE_SIZE];
+static int foo_len;
 
 static struct dentry *dfs;
 
@@ -44,11 +51,58 @@ static const struct file_operations fops = {
 	.read = my_read
 };
 
+static ssize_t foo_read(struct file *file, char __user *buff, size_t count,
+		loff_t *ppos)
+{
+	int retval = -EINVAL;
+
+	if (*ppos != 0)
+		return 0;
+
+	down(&foo_sem);
+
+	if (!copy_to_user(buff, foo_data, foo_len)) {
+		*ppos += count;
+		retval = count;
+	}
+
+	up(&foo_sem);
+	return retval;
+}
+
+static ssize_t foo_write(struct file *file, char const __user *buff,
+			size_t count, loff_t *ppos)
+{
+	int retval = -EINVAL;
+
+	if (count >= PAGE_SIZE)
+		return -EINVAL;
+
+	down(&foo_sem);
+
+	if (copy_from_user(foo_data, buff, count)) {
+		foo_len = 0;
+	} else {
+		foo_len = count;
+		retval = count;
+	}
+
+	up(&foo_sem);
+	return retval;
+}
+
+static const struct file_operations foo_fops = {
+	.owner = THIS_MODULE,
+	.read = foo_read,
+	.write = foo_write
+};
+
 static int __init my_init(void)
 {
 	dfs = debugfs_create_dir("eudyptula", NULL);
 	debugfs_create_file("id", 0666, dfs, NULL, &fops);
 	debugfs_create_u32("jiffies", 0444, dfs, (u32*)&jiffies);
+	debugfs_create_file("foo", 0644, dfs, NULL, &foo_fops);
 	pr_debug("Debugfs module loaded.");
 	return 0;
 }
